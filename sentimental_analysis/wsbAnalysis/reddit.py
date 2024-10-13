18b931f869783774316c5fad6e32ad22c539c16d
Saad Babar <saadmb@umich.edu>
2024-10-13 16:41:19 -0400

    added functions to clean finnhub data, and tests to see if functions work

--- sentimental_analysis/wsbAnalysis/reddit.py
+++ sentimental_analysis/wsbAnalysis/reddit.py
@@ -1,18 +1,43 @@
 import praw
+import re
 from .models import RedditPost
 import finnhub # Using this to get all company names with tickers
 
-
+COMMON_SUFFIXES = [
+    # can add more later
+    'inc',
+    'corp',
+    'corporation',
+    'ltd',
+    'holdings',
+    'group', 
+    'plc',
+    'co',
+    'llc'
+]
 finnhub_client = finnhub.Client(api_key='cs625mhr01qv8tfqdffgcs625mhr01qv8tfqdfg0')
+
+def clean_name(name):
+    """
+    take suffixes out of company name
+    tesla, inc will become tesla
+    """
+    name = name.lower()
+    for suffix in COMMON_SUFFIXES:
+        name = re.sub(r'\b' + suffix + r'\b', '', name)
+
+    return name.strip()
+
 
 def fetch_company_tickers():
     "Returns a dictionary of company names to tickers"
     stocks = finnhub_client.stock_symbols('US') # Returns all US tickers
-    company_tickers = {stock['description'].lower(): stock['symbol'] for stock in stocks}
+    company_tickers = {clean_name(stock['description']): stock['symbol'] for stock in stocks}
 
     return company_tickers
 
-company_tickers=fetch_company_tickers()
+
+company_tickers=fetch_company_tickers()  # Dictionary [{'description': 'apple', 'symbol': 'AAPL'}]
 
 
 def extract_stock_symbol(string):
--- sentimental_analysis/wsbAnalysis/tests.py
+++ sentimental_analysis/wsbAnalysis/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from wsbAnalysis.reddit import fetch_company_tickers, clean_name
 
 # Create your tests here.
+class FetchCompanyTickersTest(TestCase):
+    def setUp(self):
+        pass
+
+    def test_clean_company_name(self):
+        self.assertEqual(clean_name('APPLE INC'), 'apple')
+
+    def test_find_companies(self):
+        output = fetch_company_tickers()
+        self.assertIn('apple', output)
+        self.assertIn('tesla', output)
