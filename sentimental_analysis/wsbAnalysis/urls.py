ebec9ddb7e78a0d353587a4fa6e07c44b0a39a09
Saad Babar <saadmb@umich.edu>
2024-10-14 19:20:48 -0400

    added api endpoint for data that i need

--- sentimental_analysis/wsbAnalysis/urls.py
+++ sentimental_analysis/wsbAnalysis/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import RedditPostViewSet, TodayRedditPostList
+from .views import RedditPostViewSet, TodayRedditPostList, get_stock_sentiment
 from . import views
 
 # urlpatterns = [
@@ -12,5 +12,6 @@ router.register(r'posts', RedditPostViewSet)
 
 urlpatterns = [
     path('posts/today/', TodayRedditPostList.as_view(), name='today-reddit-posts'),
+    path('api/stock-sentiment/', get_stock_sentiment, name='stock-sentiment'),
     path('', include(router.urls)),
 ]
--- sentimental_analysis/wsbAnalysis/views.py
+++ sentimental_analysis/wsbAnalysis/views.py
@@ -3,6 +3,10 @@ from .models import RedditPost
 from .serializers import RedditPostSerializer
 from rest_framework import viewsets, generics
 from datetime import date
+import requests
+from django.http import JsonResponse
+from .reddit import fetch_reddit_posts, fetch_fmp_company_tickers, fetch_company_tickers, get_todays_extractions, compare_td_with_top
+from .data import fmp_key
 
 
 # Create your views here.
@@ -21,3 +25,21 @@ class TodayRedditPostList(generics.ListAPIView):
     def get_queryset(self):
         today =  date.today()
         return RedditPost.objects.filter(created_at__date=today)
+
+def get_stock_sentiment(requests):
+    # Fetch the necessary data
+    company_tickers = fetch_company_tickers()
+    fmp_company_tickers = fetch_fmp_company_tickers(fmp_key)
+    
+    # Run the Reddit post extraction
+    fetch_reddit_posts(company_tickers, fmp_company_tickers)
+    
+    # Get today's stock mentions and sentiment
+    td_extractions = get_todays_extractions()
+    
+    if td_extractions:
+        # Compare with top movers
+        result = compare_td_with_top(fmp_key, td_extractions)
+        return JsonResponse(result)  # Return result as JSON
+    else:
+        return JsonResponse({"error": "No extractions found"})
